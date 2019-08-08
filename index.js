aa1b7198c03839410a015be1eea5ab230a94d2e2
Michael Beaumont <mjboamail@gmail.com>
2019-08-08 15:46:55 +0200

    Properly verify webhook signature

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { createProbot } = require('probot')
 const { resolve } = require('probot/lib/resolver')
 const { findPrivateKey } = require('probot/lib/private-key')
 const { template } = require('./views/probot')
+const verify = require('@octokit/webhooks/verify')
 
 let probot
 
@@ -38,7 +39,21 @@ module.exports.serverless = appFn => {
     // Determine incoming webhook event type
     const name = request.get('x-github-event') || request.get('X-GitHub-Event')
     const id = request.get('x-github-delivery') || request.get('X-GitHub-Delivery')
+    const signature = request.get('x-hub-signature') || request.get('X-Hub-Signature')
 
+    const body = request.body
+    console.log(probot.options.secret)
+    console.log(JSON.stringify(body, null, 2))
+    console.log(signature)
+    const matchesSignature = verify(probot.options.secret, body, signature)
+    if (!matchesSignature) {
+      console.log(probot.options.secret)
+      console.log(JSON.stringify(body, null, 2))
+      console.log(signature)
+      console.error('signature does not match event payload and secret')
+      response.sendStatus(400)
+      return
+    }
     // Do the thing
     console.log(`Received event ${name}${request.body.action ? ('.' + request.body.action) : ''}`)
     if (name) {
@@ -46,7 +61,7 @@ module.exports.serverless = appFn => {
         await probot.receive({
           name,
           id,
-          payload: request.body
+          payload: body
         })
         response.send({
           statusCode: 200,
